bbb54715045dad458a3e7b2921aebc84d463b371
Yang Yan <ssyangy@gmail.com>
2012-03-26 22:33:38 +0800

    create

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,106 @@
+class TasksController < ApplicationController
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.where('status = 0').order("content")
+    @completed = Task.where('status = 1').order("updated_at DESC")
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tasks }
+    end
+  end        
+  
+  def complete 
+    @task = Task.find(params[:id])
+    @task.status = 1
+    @task.save
+    
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def uncomplete 
+    @task = Task.find(params[:id])
+    @task.status = 0
+    @task.save
+    
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @task }
+    end
+  end
+
+  # GET /tasks/new
+  # GET /tasks/new.json
+  def new
+    @task = Task.new
+    @task.status = 0
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @task }
+    end
+  end
+
+  # GET /tasks/1/edit
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: 'Task was successfully created.' }
+        format.json { render json: @task, status: :created, location: @task }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tasks/1
+  # PUT /tasks/1.json
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update_attributes(params[:task])
+        format.html { redirect_to tasks_url, notice: 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    respond_to do |format|
+      format.js
+    #  format.html { redirect_to tasks_url }
+    #  format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = 'bcf4e3a40df658a1ccc6bad6c3d6a289a75032df2ca6749ab36a356136c8d0a6c9efd9a8bb17af5243c00ce5c395ab4bc68993a98805615d964690e5afa7857c'
