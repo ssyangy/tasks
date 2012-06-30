56eac19de1faece5057e6798ebde3b4961c9fe48
Yang Yan <ssyangy@gmail.com>
2012-07-01 01:24:43 +0800

    up

--- db/migrate/20120319100857_create_tasks.rb
+++ db/migrate/20120319100857_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :content
+      t.string :title
+      t.text :detail
       t.integer :status,  :default => 0
 
       t.timestamps
--- db/migrate/20120620140031_add_due_to_tasks.rb
+++ db/migrate/20120620140031_add_due_to_tasks.rb
@@ -1,5 +1,6 @@
 class AddDueToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :due, :integer
+    add_column :tasks, :due_date, :date
   end
 end
